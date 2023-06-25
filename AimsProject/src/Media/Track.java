e9a972ef9b2c3344610e499e9a43846d99797d45
NQHUng2805 <105558370+NQHUng2805@users.noreply.github.com>
2023-06-25 22:35:02 +0700

    Update Track.java

--- AimsProject/src/Media/Track.java
+++ AimsProject/src/Media/Track.java
@@ -1,12 +1,57 @@
-public class Track extends CompactDisc {
-	private String title;
-	private int length;
+package AimsProject.Media;
 
-	public String getTitle(){
-	      return title;
-	}
+import AimsProject.Playable.playable;
+import java.util.Objects;
 
-	public String getLength(){
-	      return length;
-	}
+public class Track implements Playable {
+
+    private String title;
+    private int length;
+
+    public Track() {
+    }
+
+    public Track(String title, int length) {
+        this.title = title;
+        this.length = length;
+    }
+
+    public Track(String title) {
+        this.title = title;
+    }
+
+    //getters
+    public int getLength() {
+        return length;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    //interface
+    @Override
+    public void play() {
+        System.out.println("\"" + this.getTitle() + "\" length: " + this.getLength());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (o instanceof Track tmp) {
+            return tmp.length == this.length && tmp.title.equals(this.title);
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 23 * hash + Objects.hashCode(this.title);
+        return hash;
+    }
+
 }
