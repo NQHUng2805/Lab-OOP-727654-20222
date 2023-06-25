fff6b5a6775a3050aceb88761c89394dc7449db9
NQHUng2805 <105558370+NQHUng2805@users.noreply.github.com>
2023-06-25 22:40:53 +0700

    Update Disc.java

--- AimsProject/src/Media/Disc.java
+++ AimsProject/src/Media/Disc.java
@@ -1,5 +1,56 @@
+package AimsProject.Media;
+
 public class Disc extends Media {
-	private String director;
-	private float length;
+
+    private int length;
+    private String director;
+
+    public Disc() {
+        super();
+    }
+
+    public Disc(String title, String category, float cost) {
+        super(title, category, cost);
+    }
+
+    public Disc(String title, String category) {
+        super(title, category);
+    }
+
+    public Disc(String title) {
+        super(title);
+    }
+
+    public Disc(String title, int length, String director) {
+        super(title);
+        this.length = length;
+        this.director = director;
+    }
+
+    public Disc(String title, String category, int length, String director) {
+        super(title, category);
+        this.length = length;
+        this.director = director;
+    }
+
+    public Disc(String title, String category, float cost, int length, String director) {
+        super(title, category, cost);
+        this.length = length;
+        this.director = director;
+    }
+
+    public Disc(String title, String category, float cost, String director) {
+        super(title, category, cost);
+        this.director = director;
+    }
+
+    //getters
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLength() {
+        return length;
+    }
 
 }
