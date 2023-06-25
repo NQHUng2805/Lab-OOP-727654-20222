653312344b5dd86c958114a8d2973a8706de94d2
NQHUng2805 <105558370+NQHUng2805@users.noreply.github.com>
2023-06-25 22:43:32 +0700

    Update book.java

--- AimsProject/src/Media/book.java
+++ AimsProject/src/Media/book.java
@@ -1,18 +1,45 @@
-public class Book extends media{
- 
-   private List<String> authors = new ArrayList<String>();
 
-   public void addAuthor(String AuthorName){ 
-            if (!this.authors.contains(authorName)) {
-                this.authors.add(authorName);
-            }
-   }
+package AimsProject.Media;
 
-   public void removeAuthor(String AuthorName){ 
-            if (this.authors.contains(authorName)) {
-                this.authors.remove(authorName);
-            }
-   }
+import java.util.ArrayList;
 
-   public Book() {
-   }
+public class book extends Media {
+
+    private ArrayList<String> authors = new ArrayList<>();
+
+    public Book() {
+        super();
+    }
+
+    public Book(String title) {
+        super(title);
+    }
+
+    public Book(String title, String category) {
+        super(title, category);
+    }
+
+    public Book(String title, String category, float cost) {
+        super(title, category, cost);
+    }
+
+    public void addAuthor(String AuthorName) {
+        if (!this.authors.contains(AuthorName)) {
+            this.authors.add(AuthorName);
+        }
+    }
+
+    public void removeAuthor(String AuthorName) {
+        if (this.authors.contains(AuthorName)) {
+            this.authors.remove(AuthorName);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Book: "+this.getTitle()+" - Category: "+this.getCategory()+" - Price: "+this.getCost();
+                
+    }
+
+}
+    
