a84ebb0ff5f2c0d87dd3f54c858668625e003e54
NQHUng2805 <105558370+NQHUng2805@users.noreply.github.com>
2023-06-25 22:32:02 +0700

    Add files via upload

---
+++ AimsProject/src/Media/Track.java
@@ -0,0 +1,12 @@
+public class Track extends CompactDisc {
+	private String title;
+	private int length;
+
+	public String getTitle(){
+	      return title;
+	}
+
+	public String getLength(){
+	      return length;
+	}
+}
